ddf11cabd5e17676c1fe2088abbafc604d6549ea
Kolobok12309 <Kolobok12309@users.noreply.github.com>
2021-12-06 17:31:19 +0000

    deploy: f574eef743795df15763f45ce5cb40bc6a53b343

---
+++ _nuxt/3197092.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{177:function(n,e,t){"use strict";t.r(e);var l=t(38),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  Index page\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/53a3a67.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{176:function(n,e,t){"use strict";t.r(e);var l=t(38),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  About page\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
