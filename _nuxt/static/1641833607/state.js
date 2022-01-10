0ae06c489b439fe22e457367c6f12ad1c41ae249
Kolobok12309 <Kolobok12309@users.noreply.github.com>
2022-01-10 16:53:29 +0000

    deploy: 3919e1873ede4309efd98d446d45293e78a97e00

---
+++ _nuxt/static/1641833607/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvue-router-webcache\u002F_nuxt\u002Fstatic\u002F1641833607",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fvue-router-webcache\u002F",assetsPath:"\u002Fvue-router-webcache\u002F_nuxt\u002F",cdnURL:a}}}}(null));
